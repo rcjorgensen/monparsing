dd7af4dd9e5364bd557fffd4f82543b9859768f8
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2023-07-06 16:39:48 +0200

    Add tests

--- tests/MonParsing.Core.Tests/ParserTests.cs
+++ tests/MonParsing.Core.Tests/ParserTests.cs
@@ -5,20 +5,28 @@ namespace MonParsing.Core.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Item_does_not_parse_empty_string() => Parser.Item("").Should().BeEmpty();
+    public void Item_does_not_parse_empty_input() => Parser.Item("").Should().BeEmpty();
 
     [Fact]
-    public void Item_parses_one_character() => Parser.Item("foo").Should().Equal(('f', "oo"));
+    public void Item_parses_first_item() => Parser.Item("foo").Should().Equal(('f', "oo"));
 
     [Fact]
-    public void Seq_does_not_parse_empty_string() =>
+    public void Seq_does_not_parse_empty_input() =>
         Parser.Seq(Parser.Item, Parser.Item)("").Should().BeEmpty();
 
     [Fact]
-    public void Seq_does_not_parse_string_with_length_1() =>
+    public void Seq_does_not_parse_input_with_length_1() =>
         Parser.Seq(Parser.Item, Parser.Item)("f").Should().BeEmpty();
 
     [Fact]
-    public void Seq_does_parses_string_with_length_greater_than_1() =>
+    public void Seq_does_parses_input_with_length_greater_than_1() =>
         Parser.Seq(Parser.Item, Parser.Item)("foo").Should().Equal((('f', 'o'), "o"));
+
+    [Fact]
+    public void Sat_does_not_parse_input_when_predicate_is_not_satisfied() =>
+        Parser.Sat(c => c == 'f')("bar").Should().BeEmpty();
+
+    [Fact]
+    public void Sat_parses_input_when_predicate_is_satisfied() =>
+        Parser.Sat(c => c == 'f')("foo").Should().Equal(('f', "oo"));
 }
